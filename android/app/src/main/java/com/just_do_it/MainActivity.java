6587e3eafaf0e53cd39c874d9fbe95bedce8357a
arkbuilder117 <noah.matwalker@gmail.com>
2022-10-04 16:36:38 -0500

    Please work

--- android/app/src/main/java/com/just_do_it/MainActivity.java
+++ android/app/src/main/java/com/just_do_it/MainActivity.java
@@ -4,7 +4,18 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 
+// Added for react navigation
+import android.os.Bundle;
+// 
+
 public class MainActivity extends ReactActivity {
+
+  // Added for react navigation
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
+  // 
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
